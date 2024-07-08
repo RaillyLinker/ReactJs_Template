b76bf814077066e7b4a5971cf5c584b035698ba4
RaillyLinker <raillylinker@gmail.com>
2024-07-08 11:15:57 +0900

    설명글 v1 완료, 리펙토링

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className="App-logo" >안녕하세요.</div>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
