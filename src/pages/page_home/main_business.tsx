5c03d7e19ba52b798d9cdfe3711adfa7e8e0471d
RaillyLinker <raillylinker@gmail.com>
2024-07-10 10:52:06 +0900

    컴포넌트 구조 변경중

--- src/pages/page_home/main_business.tsx
+++ src/pages/page_home/main_business.tsx
@@ -1,130 +1,123 @@
 import React, { Component } from 'react';
-import MainView from './main_view';
 
 // [비즈니스 클래스]
 class MainBusiness {
-  // (페이지 View 객체)
-  private mainView: MainView;
-
   // (페이지 Props 객체)
-  private pageProps: PageProps;
+  pageProps: PageProps = {};
 
-  items: {
-    itemTitle: string;
-    itemDescription: string;
-    onItemClicked: () => void;
-  }[] =
-    [
-      {
-        itemTitle: "페이지 / 라우터 샘플 리스트",
-        itemDescription: "페이지 이동, 파라미터 전달 등의 샘플 리스트",
-        onItemClicked: (): void => {
-          console.log("페이지 / 라우터 샘플 리스트");
-        }
-      },
-      {
-        itemTitle: "다이얼로그 샘플 리스트",
-        itemDescription: "다이얼로그 호출 샘플 리스트",
-        onItemClicked: (): void => {
-          console.log("다이얼로그 호출 샘플 리스트");
-        }
-      },
-      {
-        itemTitle: "네트워크 요청 샘플 리스트",
-        itemDescription: "네트워크 요청 및 응답 처리 샘플 리스트",
-        onItemClicked: (): void => {
-          console.log("네트워크 요청 및 응답 처리 샘플 리스트");
-        }
-      },
-      {
-        itemTitle: "계정 샘플",
-        itemDescription: "계정 관련 기능 샘플",
-        onItemClicked: (): void => {
-          console.log("계정 관련 기능 샘플");
-        }
-      },
-      {
-        itemTitle: "기타 샘플 리스트",
-        itemDescription: "기타 테스트 샘플을 모아둔 리스트",
-        onItemClicked: (): void => {
-          console.log("기타 테스트 샘플을 모아둔 리스트");
-        }
-      },
-      {
-        itemTitle: "테스트",
-        itemDescription: "테스트",
-        onItemClicked: (): void => {
-          console.log("테스트");
-        }
-      },
-      {
-        itemTitle: "테스트",
-        itemDescription: "테스트",
-        onItemClicked: (): void => {
-          console.log("테스트");
-        }
-      },
-      {
-        itemTitle: "테스트",
-        itemDescription: "테스트",
-        onItemClicked: (): void => {
-          console.log("테스트");
-        }
-      },
-      {
-        itemTitle: "테스트",
-        itemDescription: "테스트",
-        onItemClicked: (): void => {
-          console.log("테스트");
-        }
-      },
-      {
-        itemTitle: "테스트",
-        itemDescription: "테스트",
-        onItemClicked: (): void => {
-          console.log("테스트");
-        }
-      },
-      {
-        itemTitle: "테스트",
-        itemDescription: "테스트",
-        onItemClicked: (): void => {
-          console.log("테스트");
-        }
-      },
-      {
-        itemTitle: "테스트",
-        itemDescription: "테스트",
-        onItemClicked: (): void => {
-          console.log("테스트");
-        }
-      },
-      {
-        itemTitle: "테스트",
-        itemDescription: "테스트",
-        onItemClicked: (): void => {
-          console.log("테스트");
-        }
-      },
-      {
-        itemTitle: "테스트",
-        itemDescription: "테스트",
-        onItemClicked: (): void => {
-          console.log("테스트");
-        }
-      }
-    ];
+  // (페이지 State 및 State 갱신자)
+  pageState: PageState = this.initPageState();
+  setPageState: React.Dispatch<React.SetStateAction<PageState>> = () => { };
 
   //----------------------------------------------------------------------------
   // [생명주기 함수]
   // (비즈니스 클래스 생성자)
-  constructor(mainView: MainView, pageProps: PageProps) {
-    this.mainView = mainView;
-    this.pageProps = pageProps;
+  constructor() {
+  }
 
-    // 페이지 State 초기화
-    this.mainView.state = {
-      counter: 0,
+  // (페이지 State 초기화)
+  private initPageState(): PageState {
+    return {
+      items: [
+        {
+          itemTitle: "페이지 / 라우터 샘플 리스트",
+          itemDescription: "페이지 이동, 파라미터 전달 등의 샘플 리스트",
+          onItemClicked: (): void => {
+            console.log("페이지 / 라우터 샘플 리스트");
+          }
+        },
+        {
+          itemTitle: "다이얼로그 샘플 리스트",
+          itemDescription: "다이얼로그 호출 샘플 리스트",
+          onItemClicked: (): void => {
+            console.log("다이얼로그 호출 샘플 리스트");
+          }
+        },
+        {
+          itemTitle: "네트워크 요청 샘플 리스트",
+          itemDescription: "네트워크 요청 및 응답 처리 샘플 리스트",
+          onItemClicked: (): void => {
+            console.log("네트워크 요청 및 응답 처리 샘플 리스트");
+          }
+        },
+        {
+          itemTitle: "계정 샘플",
+          itemDescription: "계정 관련 기능 샘플",
+          onItemClicked: (): void => {
+            console.log("계정 관련 기능 샘플");
+          }
+        },
+        {
+          itemTitle: "기타 샘플 리스트",
+          itemDescription: "기타 테스트 샘플을 모아둔 리스트",
+          onItemClicked: (): void => {
+            console.log("기타 테스트 샘플을 모아둔 리스트");
+          }
+        },
+        {
+          itemTitle: "테스트",
+          itemDescription: "테스트",
+          onItemClicked: (): void => {
+            console.log("테스트");
+          }
+        },
+        {
+          itemTitle: "테스트",
+          itemDescription: "테스트",
+          onItemClicked: (): void => {
+            console.log("테스트");
+          }
+        },
+        {
+          itemTitle: "테스트",
+          itemDescription: "테스트",
+          onItemClicked: (): void => {
+            console.log("테스트");
+          }
+        },
+        {
+          itemTitle: "테스트",
+          itemDescription: "테스트",
+          onItemClicked: (): void => {
+            console.log("테스트");
+          }
+        },
+        {
+          itemTitle: "테스트",
+          itemDescription: "테스트",
+          onItemClicked: (): void => {
+            console.log("테스트");
+          }
+        },
+        {
+          itemTitle: "테스트",
+          itemDescription: "테스트",
+          onItemClicked: (): void => {
+            console.log("테스트");
+          }
+        },
+        {
+          itemTitle: "테스트",
+          itemDescription: "테스트",
+          onItemClicked: (): void => {
+            console.log("테스트");
+          }
+        },
+        {
+          itemTitle: "테스트",
+          itemDescription: "테스트",
+          onItemClicked: (): void => {
+            console.log("테스트");
+          }
+        },
+        {
+          itemTitle: "테스트",
+          itemDescription: "테스트",
+          onItemClicked: (): void => {
+            console.log("테스트");
+          }
+        }
+      ]
     };
   }
 
@@ -133,16 +126,6 @@ class MainBusiness {
   // DOM 노드가 있어야 하는 초기화 작업은 이 메서드에서 이루어지면 됩니다.
   // 외부에서 데이터를 불러와야 한다면 네트워크 요청을 보내기 적절한 위치라고 할 수 있습니다.
   componentDidMount() {
-
-  }
-
-  // (갱신이 일어난 직후)
-  // 갱신이 일어난 직후에 호출됩니다. 최초 랜더링에서는 호출되지 않습니다.
-  // 컴포넌트가 갱신되었을 때 DOM을 조작하기 위하여 이 메서드를 활용하면 좋습니다. 
-  // 또한, 이전과 현재의 props를 비교하여 네트워크 요청을 보내는 작업도 이 메서드에서 이루어지면 됩니다.
-  // (가령, props가 변하지 않았다면 네트워크 요청을 보낼 필요가 없습니다).
-  componentDidUpdate() {
-
   }
 
   // (컴포넌트가 마운트 해제되어 제거되기 직전)
@@ -151,14 +134,10 @@ class MainBusiness {
   // 이제 컴포넌트는 다시 렌더링되지 않으므로, componentWillUnmount() 내에서 setState()를 호출하면 안 됩니다. 
   // 컴포넌트 인스턴스가 마운트 해제되고 나면, 절대로 다시 마운트되지 않습니다.
   componentWillUnmount() {
-
   }
 
   //----------------------------------------------------------------------------
   // [public 함수]
-  onCntUpBtnClick = (): void => {
-    this.mainView.setState((prevState) => ({ counter: prevState.counter + 1 }));
-  };
 
   //----------------------------------------------------------------------------
   // [private 함수]
@@ -166,12 +145,20 @@ class MainBusiness {
 
 //----------------------------------------------------------------------------
 // [페이지 State 인터페이스]
+// 페이지에서 사용할 모든 변수는 여기에 저장하여 사용하세요.
 export interface PageState {
-  counter: number;
+  items: {
+    itemTitle: string;
+    itemDescription: string;
+    onItemClicked: () => void;
+  }[]
 }
 
 // [페이지 Props 인터페이스]
 export interface PageProps {
+  // (view 와 연결되는 Business 객체)
+  // 외부에서 주입받는다면 외부의 Business 객체를 사용하고, 아니라면 내부에서 만들어서 사용합니다.
+  mainBusiness?: MainBusiness;
 }
 
 export default MainBusiness;
--- src/pages/page_home/main_view.tsx
+++ src/pages/page_home/main_view.tsx
@@ -1,60 +1,49 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './main_view.module.css';
 import MainBusiness, { PageState, PageProps } from './main_business';
 import GcHeader from '../../global_components/gc_header/main_view';
 import GcFooter from '../../global_components/gc_footer/main_view';
 import { FixedSizeList as List, ListChildComponentProps } from 'react-window';
 
-// [페이지 뷰 클래스]
-class MainView extends Component<{}, PageState> {
-  // (페이지 Business 객체)
-  private mainBusiness: MainBusiness;
+const MainView: React.FC<PageProps> = (props) => {
+  // (보일러 플레이트 코드)
+  // 페이지 Business 객체
+  const [mainBusiness] = useState(() => props.mainBusiness || new MainBusiness());
+  mainBusiness.pageProps = props;
 
-  // (뷰 클래스 생성자)
-  constructor(props: PageProps) {
-    super(props);
-    this.mainBusiness = new MainBusiness(this, props);
-  }
+  // 페이지 State 생성 및 mainBusiness 에 할당
+  const [pageState, setPageState] =
+    React.useState<PageState>(mainBusiness.pageState);
+  mainBusiness.pageState = pageState;
+  mainBusiness.setPageState = setPageState;
 
-  // (컴포넌트 생명주기 함수들)
-  // 그외 생명주기 : https://ko.legacy.reactjs.org/docs/react-component.html
-  componentDidMount() {
-    this.mainBusiness.componentDidMount();
-  }
-  componentDidUpdate() {
-    this.mainBusiness.componentDidUpdate();
-  }
-  componentWillUnmount() {
-    this.mainBusiness.componentWillUnmount();
-  }
+  // 페이지 생명주기를 mainBusiness 로 전달
+  useEffect(() => {
+    mainBusiness.componentDidMount();
+    return () => {
+      mainBusiness.componentWillUnmount();
+    }
+  }, []);
 
   //----------------------------------------------------------------------------
-  // (페이지 화면 반환 함수)
-  render(): JSX.Element {
-    const Row = ({ index, style }: ListChildComponentProps) => (
-      <div style={style} onClick={this.mainBusiness.items[index].onItemClicked}>
-        {this.mainBusiness.items[index].itemTitle}
-      </div>
-    );
+  // (페이지 화면 구성 코드)
+  const Row = ({ index, style }: ListChildComponentProps) => (
+    <div style={style} onClick={pageState.items[index].onItemClicked}>
+      {pageState.items[index].itemTitle}
+    </div>
+  );
 
-    return (
-      <div className={styles.MainView}>
-        <GcHeader headerTitle='홈' />
+  return (
+    <div className={styles.MainView}>
+      <GcHeader headerTitle='홈' />
 
-        <List
-          height={400}
-          itemCount={this.mainBusiness.items.length}
-          itemSize={35}
-          width={300}
-        >
-          {Row}
-        </List>
+      <List height={400} itemCount={pageState.items.length} itemSize={35} width={300} >
+        {Row}
+      </List>
 
-        <GcFooter footerMsg='by Railly' />
-      </div>
-    );
-  }
-}
-
+      <GcFooter footerMsg='by Railly' />
+    </div>
+  );
+};
 
 export default MainView;
