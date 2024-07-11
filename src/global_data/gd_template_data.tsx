8003e16f0fd5533f25945c44600c2d9688589169
RaillyLinker <raillylinker@gmail.com>
2024-07-11 13:12:01 +0900

    리펙토링

---
+++ src/global_data/gd_template_data.tsx
@@ -0,0 +1,6 @@
+import { Stack } from '../global_classes/gc_my_classes'
+import { StateBasic } from '../global_classes/gc_ancestor_classes'
+
+// [템플릿 전용 데이터 모음]
+// (페이지 히스토리 스택)
+export const historyStack: Stack<StateBasic> = new Stack();
