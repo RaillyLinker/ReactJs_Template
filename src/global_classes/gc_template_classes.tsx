802a0cad707ae2b0e8199cd875ea1872207d7253
RaillyLinker <raillylinker@gmail.com>
2024-07-17 17:56:30 +0900

    히스토리 기능을 위한 리펙토링

--- src/global_classes/gc_template_classes.tsx
+++ src/global_classes/gc_template_classes.tsx
@@ -2,21 +2,18 @@
 // (컴포넌트 State 클래스 기본타입)
 export interface StateBasic { }
 
+// (페이지 히스토리 VO 클래스)
 export class PageHistory {
-    // (히스토리 인덱스)
-    // window.history.state["idx"] 로 가져온 히스토리 인덱스
-    historyIdx: number;
+  // (히스토리 키)
+  // window.history.state["key"] 로 가져온 히스토리 고유키
+  historyKey: string;
 
-    // (히스토리 키)
-    // window.history.state["key"] 로 가져온 히스토리 고유키
-    historyKey: string;
+  // (Page State)
+  pageState: StateBasic;
 
-    // (Page State)
-    pageState: StateBasic;
-
-    constructor(historyIdx: number, historyKey: string, pageState: StateBasic) {
-        this.historyIdx = historyIdx;
-        this.historyKey = historyKey;
-        this.pageState = pageState;
-    }
+  // (클래스 생성자)
+  constructor(historyKey: string, pageState: StateBasic) {
+    this.historyKey = historyKey;
+    this.pageState = pageState;
+  }
 }
--- src/global_data/gd_template_data.tsx
+++ src/global_data/gd_template_data.tsx
@@ -1,6 +1,5 @@
-import { Stack } from '../global_classes/gc_my_classes'
-import { StateBasic } from '../global_classes/gc_template_classes'
+import { PageHistory } from '../global_classes/gc_template_classes';
 
 // [템플릿 전용 데이터 모음]
-// (페이지 히스토리 스택)
-export const historyStack: Stack<StateBasic> = new Stack();
+// (페이지 히스토리 딕셔너리)
+export const pageHistoryDict: { [idx: string]: PageHistory } = {};
