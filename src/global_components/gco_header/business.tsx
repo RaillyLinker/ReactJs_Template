fd55660359a755b2723e97ae4b7787b4dae3b2da
RaillyLinker <raillylinker@gmail.com>
2024-07-19 17:51:53 +0900

    리펙토링

--- src/global_components/gco_header/business.tsx
+++ src/global_components/gco_header/business.tsx
@@ -3,6 +3,7 @@ import { NavigateFunction } from 'react-router-dom';
 import cloneDeep from 'lodash/cloneDeep';
 
 // [비즈니스 클래스]
+// 함수는 변수 형식으로 저장합시다. 그래야 onclick 에 입력시 에러가 나지 않습니다.
 class Business {
   // (컴포넌트 State)
   // 컴포넌트 ViewModel 입니다.
@@ -30,7 +31,7 @@ class Business {
   // 컴포넌트가 마운트된 직 후에 호출됩니다. 
   // DOM 노드가 있어야 하는 초기화 작업은 이 메서드에서 이루어지면 됩니다.
   // 외부에서 데이터를 불러와야 한다면 네트워크 요청을 보내기 적절한 위치라고 할 수 있습니다.
-  onComponentDidMount() {
+  onComponentDidMount = () => {
   }
 
   // (컴포넌트가 마운트 해제되어 제거되기 직전)
@@ -38,14 +39,14 @@ class Business {
   // 이 메서드 내에서 타이머 제거, 네트워크 요청 취소, componentDidMount() 내에서 생성된 구독 해제 등 필요한 모든 정리 작업을 수행하세요.
   // 이제 컴포넌트는 다시 렌더링되지 않으므로, componentWillUnmount() 내에서 setState()를 호출하면 안 됩니다. 
   // 컴포넌트 인스턴스가 마운트 해제되고 나면, 절대로 다시 마운트되지 않습니다.
-  onComponentWillUnmount() {
+  onComponentWillUnmount = () => {
   }
 
   //----------------------------------------------------------------------------
   // [public 함수]
   // (컴포넌트 화면 랜더링 함수)
   // 이 함수를 호출하면 State 정보에 맞게 화면이 갱신됩니다.
-  reRender() {
+  reRender = () => {
     if (this.mainState != undefined) {
       this.setMainState!(cloneDeep(this.mainState));
     }
--- src/pages/page_test/business.tsx
+++ src/pages/page_test/business.tsx
@@ -8,6 +8,7 @@ import GcoHeaderBusiness from '../../global_components/gco_header/business';
 import GcoFooterBusiness from '../../global_components/gco_footer/business';
 
 // [비즈니스 클래스]
+// 함수는 변수 형식으로 저장합시다. 그래야 onclick 에 입력시 에러가 나지 않습니다.
 // 본 클래스의 객체는 다른 페이지로 이동했다가 복귀하더라도 그대로 유지됩니다.
 class Business implements BusinessBasic {
   // (페이지 파라미터)
@@ -34,12 +35,12 @@ class Business implements BusinessBasic {
   // 컴포넌트 진입시 가장 먼저 실행됩니다.
   // this.pathParams, this.queryParams, this.mainState 를 입력하면 되며,
   // 만약 하나라도 undefined 이라면 에러 화면이 나오게 됩니다.
-  initMainState(
+  initMainState = (
     // Path 파라미터 객체 (ex : pathParams["testPath"])
     pathParams: Readonly<Params<string>>,
     // Query 파라미터 객체 (ex : queryParams.get("testQuery"))
     queryParams: URLSearchParams
-  ) {
+  ) => {
     // Path 파라미터 객체로 값 입력하기
     // (ex : pathParams["testPath"])
     this.pathParams = {};
@@ -161,7 +162,7 @@ class Business implements BusinessBasic {
   // 컴포넌트가 마운트된 직 후에 호출됩니다. 
   // DOM 노드가 있어야 하는 초기화 작업은 이 메서드에서 이루어지면 됩니다.
   // 외부에서 데이터를 불러와야 한다면 네트워크 요청을 보내기 적절한 위치라고 할 수 있습니다.
-  onComponentDidMount() {
+  onComponentDidMount = () => {
   }
 
   // (컴포넌트가 마운트 해제되어 제거되기 직전)
@@ -169,14 +170,14 @@ class Business implements BusinessBasic {
   // 이 메서드 내에서 타이머 제거, 네트워크 요청 취소, componentDidMount() 내에서 생성된 구독 해제 등 필요한 모든 정리 작업을 수행하세요.
   // 이제 컴포넌트는 다시 렌더링되지 않으므로, componentWillUnmount() 내에서 setState()를 호출하면 안 됩니다. 
   // 컴포넌트 인스턴스가 마운트 해제되고 나면, 절대로 다시 마운트되지 않습니다.
-  onComponentWillUnmount() {
+  onComponentWillUnmount = () => {
   }
 
   //----------------------------------------------------------------------------
   // [public 함수]
   // (컴포넌트 화면 랜더링 함수)
   // 이 함수를 호출하면 State 정보에 맞게 화면이 갱신됩니다.
-  reRender() {
+  reRender = () => {
     if (this.mainState != undefined) {
       this.setMainState!(cloneDeep(this.mainState));
     }
