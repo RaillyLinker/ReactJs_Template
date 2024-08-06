b83cea0d1ae5a98375e52079b8f5ca5ded5b9fb8
RaillyLinker <raillylinker@gmail.com>
2024-08-06 14:20:47 +0900

    헤더 수정

--- src/global_components/gco_header/view.tsx
+++ src/global_components/gco_header/view.tsx
@@ -28,7 +28,14 @@ const View: React.FC<Props> = (props) => {
   return (
     <div className={styles.MainView}>
       <header>
-        <h1 id={styles.HeaderTitle}>{mainBusiness.headerTitle}</h1>
+        <h1 id={styles.HeaderTitleContainer}>
+          <img
+            src="/logo192.png"
+            alt="Logo"
+            id={styles.GoToHomeLogo}
+          />
+          <span id={styles.HeaderTitle}>{mainBusiness.headerTitle}</span>
+        </h1>
       </header>
     </div>
   );
