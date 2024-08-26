6c541480bb35bdcd8354a58a499e9ab90da9278a
RaillyLinker <raillylinker@gmail.com>
2024-08-26 16:06:00 +0900

    기본 네트워크 요청 샘플 추가

--- src/pages/page_basicNetworkRequestSampleList/business.tsx
+++ src/pages/page_basicNetworkRequestSampleList/business.tsx
@@ -86,14 +86,22 @@ class Business extends PageBusinessBasic {
       },
       {
         uid: 5,
-        itemTitle: "Post 메소드 요청 샘플 5 (application/json - Object Param)",
-        itemDescription: "Post 요청 테스트 (Request Body - Object Parameter 받기)",
+        itemTitle: "Post 메소드 요청 샘플 5 (multipart/form-data - File List)",
+        itemDescription: "Post 메소드 요청 File List Parameter (multipart/form-data)",
         onItemClicked: (): void => {
           this.navigate("/network-sample-list/basic-network-request-sample/post-request-sample5");
         }
       },
       {
         uid: 6,
+        itemTitle: "Post 메소드 요청 샘플 6 (application/json - Object Param)",
+        itemDescription: "Post 요청 테스트 (Request Body - Object Parameter 받기)",
+        onItemClicked: (): void => {
+          this.navigate("/network-sample-list/basic-network-request-sample/post-request-sample6");
+        }
+      },
+      {
+        uid: 7,
         itemTitle: "네트워크 요청 에러 발생 테스트",
         itemDescription: "네트워크 요청에 500 에러가 발생했을 시에 대한 테스트",
         onItemClicked: (): void => {
@@ -101,7 +109,7 @@ class Business extends PageBusinessBasic {
         }
       },
       {
-        uid: 7,
+        uid: 8,
         itemTitle: "String 타입 응답 테스트",
         itemDescription: "text/string 타입의 응답값을 반환하는 API 에 대한 테스트",
         onItemClicked: (): void => {
@@ -109,7 +117,7 @@ class Business extends PageBusinessBasic {
         }
       },
       {
-        uid: 8,
+        uid: 9,
         itemTitle: "HTML 타입 응답 테스트",
         itemDescription: "text/html 타입의 응답값을 반환하는 API 에 대한 테스트",
         onItemClicked: (): void => {
@@ -117,7 +125,7 @@ class Business extends PageBusinessBasic {
         }
       },
       {
-        uid: 9,
+        uid: 10,
         itemTitle: "네트워크 파일 다운로드 테스트",
         itemDescription: "네트워크 파일 다운로드 테스트",
         onItemClicked: (): void => {
